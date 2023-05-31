5b7dbbad6a99b899cc91c85f74c9a8b8aa1c5467
Chris Jasper <chris@chrisjasper.uk>
2023-05-31 22:44:55 +0100

    Build and print the board

--- src/main.rs
+++ src/main.rs
@@ -115,42 +115,82 @@ fn generate_piece_set(colour: Colour) -> Vec<Piece> {
     new_piece_set
 }
 
+fn generate_board() -> [[Square; 8]; 8] {
+    [
+        [
+            Square::Piece(Piece{colour: Colour::Black, kind: PieceKind::Rook}),
+            Square::Piece(Piece{colour: Colour::Black, kind: PieceKind::Knight}),
+            Square::Piece(Piece{colour: Colour::Black, kind: PieceKind::Bishop}),
+            Square::Piece(Piece{colour: Colour::Black, kind: PieceKind::King}),
+            Square::Piece(Piece{colour: Colour::Black, kind: PieceKind::Queen}),
+            Square::Piece(Piece{colour: Colour::Black, kind: PieceKind::Bishop}),
+            Square::Piece(Piece{colour: Colour::Black, kind: PieceKind::Knight}),
+            Square::Piece(Piece{colour: Colour::Black, kind: PieceKind::Rook}),
+        ],
+        [
+            Square::Piece(Piece{colour: Colour::Black, kind: PieceKind::Pawn}),
+            Square::Piece(Piece{colour: Colour::Black, kind: PieceKind::Pawn}),
+            Square::Piece(Piece{colour: Colour::Black, kind: PieceKind::Pawn}),
+            Square::Piece(Piece{colour: Colour::Black, kind: PieceKind::Pawn}),
+            Square::Piece(Piece{colour: Colour::Black, kind: PieceKind::Pawn}),
+            Square::Piece(Piece{colour: Colour::Black, kind: PieceKind::Pawn}),
+            Square::Piece(Piece{colour: Colour::Black, kind: PieceKind::Pawn}),
+            Square::Piece(Piece{colour: Colour::Black, kind: PieceKind::Pawn}),
+        ],
+        [
+            Square::Empty, Square::Empty, Square::Empty, Square::Empty,
+            Square::Empty, Square::Empty, Square::Empty, Square::Empty,
+        ],
+        [
+            Square::Empty, Square::Empty, Square::Empty, Square::Empty,
+            Square::Empty, Square::Empty, Square::Empty, Square::Empty,
+        ],
+        [
+            Square::Empty, Square::Empty, Square::Empty, Square::Empty,
+            Square::Empty, Square::Empty, Square::Empty, Square::Empty,
+        ],
+        [
+            Square::Empty, Square::Empty, Square::Empty, Square::Empty,
+            Square::Empty, Square::Empty, Square::Empty, Square::Empty,
+        ],
+        [
+            Square::Piece(Piece{colour: Colour::White, kind: PieceKind::Pawn}),
+            Square::Piece(Piece{colour: Colour::White, kind: PieceKind::Pawn}),
+            Square::Piece(Piece{colour: Colour::White, kind: PieceKind::Pawn}),
+            Square::Piece(Piece{colour: Colour::White, kind: PieceKind::Pawn}),
+            Square::Piece(Piece{colour: Colour::White, kind: PieceKind::Pawn}),
+            Square::Piece(Piece{colour: Colour::White, kind: PieceKind::Pawn}),
+            Square::Piece(Piece{colour: Colour::White, kind: PieceKind::Pawn}),
+            Square::Piece(Piece{colour: Colour::White, kind: PieceKind::Pawn}),
+        ],
+        [
+            Square::Piece(Piece{colour: Colour::White, kind: PieceKind::Rook}),
+            Square::Piece(Piece{colour: Colour::White, kind: PieceKind::Knight}),
+            Square::Piece(Piece{colour: Colour::White, kind: PieceKind::Bishop}),
+            Square::Piece(Piece{colour: Colour::White, kind: PieceKind::Queen}),
+            Square::Piece(Piece{colour: Colour::White, kind: PieceKind::King}),
+            Square::Piece(Piece{colour: Colour::White, kind: PieceKind::Bishop}),
+            Square::Piece(Piece{colour: Colour::White, kind: PieceKind::Knight}),
+            Square::Piece(Piece{colour: Colour::White, kind: PieceKind::Rook}),
+        ],
+    ]
+}
+
 fn main() {
     let my_colour = Colour::White;
 
     println!("My colour is: {}", my_colour);
 
     let my_pieces = generate_piece_set(Colour::White);
+    let opp_pieces = generate_piece_set(Colour::Black);
 
     for piece in my_pieces {
         println!("{} ", piece);
     }
 
-    return;
+    let mut board = generate_board();
 
-    let mut board: [[char; 8]; 8] = [
-        ['R', 'N', 'B', 'K', 'Q', 'B', 'N', 'R'],
-        ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
-        ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
-    ];
-
-    print_board(board);
-
-    println!();
-    println!();
-
-    // Move the E2 pawn forward 2 squares
-    board[RANK_2][FILE_E] = ' ';
-    board[RANK_4][FILE_E] = 'P';
-    // board[6][4] = ' ';
-    // board[5][4] = 'P';
-
-    print_board(board);
+    print_board(&board);
 
     //
     // println!("Welcome to chess!");
@@ -179,11 +219,13 @@ fn main() {
     // println!("The coin toss is: {coin_toss}");
 }
 
-fn print_board(board: [[char; 8]; 8]) {
+fn print_board(board: &[[Square; 8]; 8]) {
     for rank in board {
         for square in rank {
-            print!("{} ", square);
+            print!("| ");
+            // print!("{square:14} | ");
+            print!("{square:<width$}", square=square.to_string(), width=14);
         }
-        println!();
+        println!("|");
     }
 }
